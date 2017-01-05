6ccf1bc5174e42dfd36c6befc4cfd52a331c1eb9
Shivam Kapoor <mail@shivamkapoor.com>
2017-01-05 20:58:26 +0530

    WarmupII: soln. & test cases for stringTimes

--- src/main/scala/com/codingkapoor/codingbat/WarmupII.scala
+++ src/main/scala/com/codingkapoor/codingbat/WarmupII.scala
@@ -1,5 +1,8 @@
 package com.codingkapoor.codingbat
 
 object WarmupII {
-  
+
+  def stringTimes(str: String, n: Int) = {
+    str * n
+  }
 }
--- src/test/scala/com/codingkapoor/codingbat/WarmupIISpec.scala
+++ src/test/scala/com/codingkapoor/codingbat/WarmupIISpec.scala
@@ -5,4 +5,9 @@ import org.scalatest.FlatSpec
 
 class WarmupIISpec extends FlatSpec with Matchers {
 
+  "stringTimes" should "return a larger string that is 'n' copies of the original string, given a string and a non-negative int 'n'" in {
+    WarmupII.stringTimes("Hi", 2) should equal("HiHi")
+    WarmupII.stringTimes("Hi", 3) should equal("HiHiHi")
+    WarmupII.stringTimes("Hi", 1) should equal("Hi")
+  }
 }
