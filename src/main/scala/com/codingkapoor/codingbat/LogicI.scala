f331c4dc7d46fa3e9687046129997a54aa580790
m0dernm0nk <mail@shivamkapoor.com>
2017-01-14 12:05:20 +0530

    soln. & test cases for dateFashion

--- src/main/scala/com/codingkapoor/codingbat/LogicI.scala
+++ src/main/scala/com/codingkapoor/codingbat/LogicI.scala
@@ -2,7 +2,11 @@ package com.codingkapoor.codingbat
 
 object LogicI {
 
-  def cigarParty(cigars: Int, isWeekend: Boolean) = {
+  def cigarParty(cigars: Int, isWeekend: Boolean): Boolean = {
     if (isWeekend) cigars >= 40 else cigars >= 40 && cigars <= 60
+  }
+
+  def dateFashion(you: Int, date: Int): Int = {
+    if (you >= 8 || date >= 8) 2 else if (you <= 2 || date <= 2) 0 else 1
   }
 }
--- src/test/scala/com/codingkapoor/codingbat/LogicISpec.scala
+++ src/test/scala/com/codingkapoor/codingbat/LogicISpec.scala
@@ -10,4 +10,10 @@ class LogicISpec extends FlatSpec with Matchers {
     LogicI.cigarParty(50, false) should equal(true)
     LogicI.cigarParty(70, true) should equal(true)
   }
+
+  """You and your date are trying to get a table at a restaurant. The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes. The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, the method "dateFashion"""" should """return 2(yes) as the result, with the exception that if either of you has style of 2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe).""" in {
+    LogicI.dateFashion(5, 10) should equal(2)
+    LogicI.dateFashion(5, 2) should equal(0)
+    LogicI.dateFashion(5, 5) should equal(1)
+  }
 }
