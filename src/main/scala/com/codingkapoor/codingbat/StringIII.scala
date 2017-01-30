e9490f0a3d0fa6a95e53a120298c21802388c49a
codingkapoor <mailtoshivamk@gmail.com>
2017-01-30 17:53:57 +0530

    soln. & test cases for countYZ, withoutString, equalIsNot, gHappy

--- src/main/scala/com/codingkapoor/codingbat/StringIII.scala
+++ src/main/scala/com/codingkapoor/codingbat/StringIII.scala
@@ -2,4 +2,26 @@ package com.codingkapoor.codingbat
 
 object StringIII {
 
+  def countYZ(str: String): Int = {
+    str.split(" ").map(_.toLowerCase).count(x => x.endsWith("y") || x.endsWith("z"))
+  }
+
+  def withoutString(base: String, remove: String): String = {
+    val pattern = s"$remove".r
+    pattern.replaceAllIn(base, "")
+  }
+
+  def equalIsNot(str: String): Boolean = {
+    val res = str.split(" ").map(_.toLowerCase).mkString
+
+    val pattern1 = "is".r
+    val pattern2 = "not".r
+    pattern1.findAllIn(res).size == pattern2.findAllIn(res).size
+  }
+
+  def gHappy(str: String): Boolean = {
+    val pattern = "[^g]g[^g]".r
+    pattern.findAllIn(str).isEmpty
+  }
+
 }
--- src/test/scala/com/codingkapoor/codingbat/StringIIISpec.scala
+++ src/test/scala/com/codingkapoor/codingbat/StringIIISpec.scala
@@ -4,4 +4,27 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class StringIIISpec extends FlatSpec with Matchers {
 
+  """Given a string, method "countXZ"""" should """return the count of the number of words ending in 'y' or 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count, but not the 'y' in "yellow".""" in {
+    StringIII.countYZ("fez day") should equal(2)
+    StringIII.countYZ("day fez") should equal(2)
+    StringIII.countYZ("day fyyyz") should equal(2)
+  }
+
+  """Given two strings, base and remove, method "withoutString"""" should """return a version of the base string where all instances of the remove string have been removed (not case sensitive). You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".""" in {
+    StringIII.withoutString("Hello there", "llo") should equal("He there")
+    StringIII.withoutString("Hello there", "e") should equal("Hllo thr")
+    StringIII.withoutString("Hello there", "x") should equal("Hello there")
+  }
+
+  """Given a string, method "equalIsNot"""" should """return true if the number of appearances of "is" anywhere in the string is equal to the number of appearances of "not" anywhere in the string (case sensitive).""" in {
+    StringIII.equalIsNot("This is not") should equal(false)
+    StringIII.equalIsNot("This is notnot") should equal(true)
+    StringIII.equalIsNot("noisxxnotyynotxisi") should equal(true)
+  }
+
+  """We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right. Method "gHappy"""" should """return true if all the g's in the given string are happy.""" in {
+    StringIII.gHappy("xxggxx") should equal(true)
+    StringIII.gHappy("xxgxx") should equal(false)
+    StringIII.gHappy("xxggyygxx") should equal(false)
+  }
 }
