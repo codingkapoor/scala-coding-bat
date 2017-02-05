d914da87c4807cc6473bd7a427354b94bbedd616
codingkapoor <mailtoshivamk@gmail.com>
2017-02-05 17:14:14 +0530

    soln. & test cases for ArrayII

---
+++ src/main/scala/com/codingkapoor/codingbat/ArrayII.scala
@@ -0,0 +1,43 @@
+package com.codingkapoor.codingbat
+
+object ArrayII {
+
+  def countEvens(nums: Array[Int]): Int = {
+    nums.count(_ % 2 == 0)
+  }
+
+  def bigDiff(nums: Array[Int]): Int = {
+    nums.max - nums.min
+  }
+
+  def centeredAverage(nums: Array[Int]): Int = {
+    val arr = nums.sorted
+    arr.slice(1, arr.length - 1).sum / (arr.length - 2)
+  }
+
+  def sum13(nums: Array[Int]): Int = {
+    if (nums.length == 0) 0
+    else nums.filter(_ < 13).sum
+  }
+
+  def sum67(nums: Array[Int]): Int = {
+    val pattern = "6[0-9]*7".r
+    pattern.replaceAllIn(nums.mkString, "").toArray.map(_.toString).map(_.toInt).sum
+  }
+
+  def has22(nums: Array[Int]): Boolean = {
+    nums.iterator.sliding(2).toList.contains(List(2, 2))
+  }
+
+  def lucky13(nums: Array[Int]): Boolean = {
+    !nums.contains(1) && !nums.contains(3)
+  }
+
+  def sum28(nums: Array[Int]): Boolean = {
+    nums.filter(_ == 2).sum == 8
+  }
+
+  def more14(nums: Array[Int]): Boolean = {
+    nums.count(_ == 1) > nums.count(_ == 4)
+  }
+}
---
+++ src/test/scala/com/codingkapoor/codingbat/ArrayIISpec.scala
@@ -0,0 +1,60 @@
+package com.codingkapoor.codingbat
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class ArrayIISpec extends FlatSpec with Matchers {
+
+  """Method "countEvens"""" should """return the number of even ints in the given array.""" in {
+    ArrayII.countEvens(Array(2, 1, 2, 3, 4)) should equal(3)
+    ArrayII.countEvens(Array(2, 2, 0)) should equal(3)
+    ArrayII.countEvens(Array(1, 3, 5)) should equal(0)
+  }
+
+  """Given an array length 1 or more of ints, method "bigDiff"""" should """return the difference between the largest and smallest values in the array.""" in {
+    ArrayII.bigDiff(Array(10, 3, 5, 6)) should equal(7)
+    ArrayII.bigDiff(Array(7, 2, 10, 9)) should equal(8)
+    ArrayII.bigDiff(Array(2, 10, 7, 2)) should equal(8)
+  }
+
+  """""" should """""" in {
+    ArrayII.centeredAverage(Array(1, 2, 3, 4, 100)) should equal(3)
+    ArrayII.centeredAverage(Array(1, 1, 5, 5, 10, 8, 7)) should equal(5)
+    ArrayII.centeredAverage(Array(-10, -4, -2, -4, -2, 0)) should equal(-3)
+  }
+
+  """Method "sum13"""" should """return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.""" in {
+    ArrayII.sum13(Array(1, 2, 2, 1)) should equal(6)
+    ArrayII.sum13(Array(1, 1)) should equal(2)
+    ArrayII.sum13(Array(1, 2, 2, 1, 13)) should equal(6)
+  }
+
+  """Method "sum67"""" should """return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.""" in {
+    ArrayII.sum67(Array(1, 2, 2)) should equal(5)
+    ArrayII.sum67(Array(1, 2, 2, 6, 99, 99, 7)) should equal(5)
+    ArrayII.sum67(Array(1, 1, 6, 7, 2)) should equal(4)
+  }
+
+  """Given an array of ints, method "has22"""" should """return true if the array contains a 2 next to a 2 somewhere.""" in {
+    ArrayII.has22(Array(1, 2, 2)) should equal(true)
+    ArrayII.has22(Array(1, 2, 1, 2)) should equal(false)
+    ArrayII.has22(Array(2, 1, 2)) should equal(false)
+  }
+
+  """Given an array of ints, method "lucky13"""" should """return true if the array contains no 1's and no 3's.""" in {
+    ArrayII.lucky13(Array(0, 2, 4)) should equal(true)
+    ArrayII.lucky13(Array(1, 2, 3)) should equal(false)
+    ArrayII.lucky13(Array(1, 2, 4)) should equal(false)
+  }
+
+  """Given an array of ints, method "sum28"""" should """return true if the sum of all the 2's in the array is exactly 8.""" in {
+    ArrayII.sum28(Array(2, 3, 2, 2, 4, 2)) should equal(true)
+    ArrayII.sum28(Array(2, 3, 2, 2, 4, 2, 2)) should equal(false)
+    ArrayII.sum28(Array(1, 2, 3, 4)) should equal(false)
+  }
+
+  """Given an array of ints, method "more14"""" should """return true if the number of 1's is greater than the number of 4's""" in {
+    ArrayII.more14(Array(1, 4, 1)) should equal(true)
+    ArrayII.more14(Array(1, 4, 1, 4)) should equal(false)
+    ArrayII.more14(Array(1, 1)) should equal(true)
+  }
+}
