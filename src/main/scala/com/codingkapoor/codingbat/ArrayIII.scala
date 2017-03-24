7e186c2f7b512457c4747c4b7e54571dd24654df
codingkapoor <mailtoshivamk@gmail.com>
2017-03-24 17:18:22 +0530

    fix34 soln. & test cases

--- src/main/scala/com/codingkapoor/codingbat/ArrayIII.scala
+++ src/main/scala/com/codingkapoor/codingbat/ArrayIII.scala
@@ -1,7 +1,26 @@
 package com.codingkapoor.codingbat
 
+import scala.annotation.tailrec
+
 object ArrayIII {
+
   def maxSpan(arr: Array[Int]): Int = {
     arr.distinct.map { x => arr.lastIndexOf(x) - arr.indexOf(x) }.max + 1
   }
+
+  def fix34(arr: Array[Int]) = {
+    val k1 = arr.zipWithIndex.filter { x => x._1 == 3 }
+    val k2 = arr.zipWithIndex.filter { x => x._1 == 4 }
+    val ls = k1.zip(k2)
+
+    @tailrec
+    def fix34R(res: List[Int], ls: List[((Int, Int), (Int, Int))]): List[Int] = ls match {
+      case Nil => res
+      case ((3, p), (4, q)) :: Nil => res.updated(q, res(p + 1)).updated(p + 1, 4)
+      case ((3, p), (4, q)) :: xs => fix34R(res.updated(q, res(p + 1)).updated(p + 1, 4), xs)
+    }
+
+    fix34R(arr.toList, ls.toList).toArray
+  }
+
 }
--- src/test/scala/com/codingkapoor/codingbat/ArrayIIISpec.scala
+++ src/test/scala/com/codingkapoor/codingbat/ArrayIIISpec.scala
@@ -8,4 +8,10 @@ class ArrayIIISpec extends FlatSpec with Matchers {
     ArrayIII.maxSpan(Array(1, 4, 2, 1, 4, 1, 4)) should equal(6)
     ArrayIII.maxSpan(Array(1, 4, 2, 1, 4, 4, 4)) should equal(6)
   }
+
+  """Method fix34""" should """return an array that contains exactly the same numbers as the given array, but rearranged so that every 3 is immediately followed by a 4. Do not move the 3's, but every other number may move. The array contains the same number of 3's and 4's, every 3 has a number after it that is not a 3, and a 3 appears in the array before any 4.""" in {
+    ArrayIII.fix34(Array(1, 3, 4, 1)) should equal(Array(1, 3, 4, 1))
+    ArrayIII.fix34(Array(1, 3, 1, 4, 4, 3, 1)) should equal(Array(1, 3, 4, 1, 1, 3, 4))
+    ArrayIII.fix34(Array(3, 2, 2, 4)) should equal(Array(3, 4, 2, 2))
+  }
 }
