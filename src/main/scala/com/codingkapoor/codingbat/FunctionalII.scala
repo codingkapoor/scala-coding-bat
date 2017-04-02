d89cb95e0ad201241c81da9342708eb32f61ad6b
codingkapoor <mailtoshivamk@gmail.com>
2017-04-03 00:38:56 +0530

    soln. & test cases for noNeg

--- src/main/scala/com/codingkapoor/codingbat/FunctionalII.scala
+++ src/main/scala/com/codingkapoor/codingbat/FunctionalII.scala
@@ -2,4 +2,8 @@ package com.codingkapoor.codingbat
 
 object FunctionalII {
 
+  def noNeg(nums: List[Int]): List[Int] = {
+    nums.filter(_ >= 0)
+  }
+
 }
--- src/test/scala/com/codingkapoor/codingbat/FunctionalIISpec.scala
+++ src/test/scala/com/codingkapoor/codingbat/FunctionalIISpec.scala
@@ -4,4 +4,10 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class FunctionalIISpec extends FlatSpec with Matchers {
 
+  """Given a list of integers, method "noNeg"""" should """return a list of the integers, omitting any that are less than 0.""" in {
+    FunctionalII.noNeg(List(1, -2)) should equal(List(1))
+    FunctionalII.noNeg(List(-3, -3, 3, 3)) should equal(List(3, 3))
+    FunctionalII.noNeg(List(-1, -1, -1)) should equal(List())
+  }
+
 }
