219e3663491bd0b209577d8941c1db40e1f17a0e
codingkapoor <mailtoshivamk@gmail.com>
2017-04-08 20:17:02 +0530

    soln. & test cases for groupSum, groupSum6

--- src/main/scala/com/codingkapoor/codingbat/RecursionII.scala
+++ src/main/scala/com/codingkapoor/codingbat/RecursionII.scala
@@ -2,4 +2,17 @@ package com.codingkapoor.codingbat
 
 object RecursionII {
 
+  def groupSum(start: Int, ls: List[Int], target: Int) = {
+    (1 to ls.size).exists(y => ls.drop(start).combinations(y).exists(x => x.sum == target))
+
+  }
+
+  def groupSum2(start: Int, ls: List[Int], target: Int) = {
+    (1 to ls.size).exists(y => ls.drop(start).permutations.toList.map(x => x.take(y)).map(_.sorted).distinct.exists(x => x.take(y).sum == target))
+  }
+
+  def groupSum6(start: Int, ls: List[Int], target: Int) = {
+    (1 to ls.size).exists(y => ls.drop(start).combinations(y).filter(x => x.count(_ == 6) == ls.count(_ == 6)).exists(x => x.sum == target))
+  }
+
 }
--- src/test/scala/com/codingkapoor/codingbat/RecursionIISpec.scala
+++ src/test/scala/com/codingkapoor/codingbat/RecursionIISpec.scala
@@ -4,4 +4,19 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class RecursionIISpec extends FlatSpec with Matchers {
 
+  """Given an array of ints, method "groupSum"""" should """evaluate if it is possible to choose a group of some of the ints, such that the group sums to the given target?""" in {
+    RecursionII.groupSum(0, List(2, 4, 8), 10) should equal(true)
+    RecursionII.groupSum(0, List(2, 4, 8), 14) should equal(true)
+    RecursionII.groupSum(0, List(2, 4, 8), 9) should equal(false)
+    RecursionII.groupSum2(0, List(2, 4, 8), 10) should equal(true)
+    RecursionII.groupSum2(0, List(2, 4, 8), 14) should equal(true)
+    RecursionII.groupSum2(0, List(2, 4, 8), 9) should equal(false)
+  }
+
+  """Given an array of ints, method "groupSum6"""" should """evaluate if it is possible to choose a group of some of the ints, beginning at the start index, such that the group sums to the given target? However, with the additional constraint that all 6's must be chosen.""" in {
+    RecursionII.groupSum6(0, List(5, 6, 2), 8) should equal(true)
+    RecursionII.groupSum6(0, List(5, 6, 2), 9) should equal(false)
+    RecursionII.groupSum6(0, List(5, 6, 2), 7) should equal(false)
+  }
+
 }
