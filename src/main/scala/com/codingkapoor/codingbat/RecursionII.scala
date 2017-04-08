14ce87942abf95c6a7324404d5433c8082b3bfdd
codingkapoor <mailtoshivamk@gmail.com>
2017-04-08 21:30:56 +0530

    soln. & test cases groupNoAdj

--- src/main/scala/com/codingkapoor/codingbat/RecursionII.scala
+++ src/main/scala/com/codingkapoor/codingbat/RecursionII.scala
@@ -15,4 +15,10 @@ object RecursionII {
     (1 to ls.size).exists(y => ls.drop(start).combinations(y).filter(x => x.count(_ == 6) == ls.count(_ == 6)).exists(x => x.sum == target))
   }
 
+  def groupNoAdj(start: Int, ls: List[Int], target: Int) = {
+    val ls1 = ls.drop(start).zipWithIndex
+    val ls2 = List(ls1.filter(x => x._2 % 2 == 0).map(_._1), ls1.filter(x => x._2 % 2 != 0).map(_._1))
+    (1 to ls.size).exists(y => ls2.exists(t => t.combinations(y).exists(x => x.sum == target)))
+  }
+
 }
--- src/test/scala/com/codingkapoor/codingbat/RecursionIISpec.scala
+++ src/test/scala/com/codingkapoor/codingbat/RecursionIISpec.scala
@@ -19,4 +19,10 @@ class RecursionIISpec extends FlatSpec with Matchers {
     RecursionII.groupSum6(0, List(5, 6, 2), 7) should equal(false)
   }
 
+  """Given an array of ints, method "groupNoAdj"""" should """evaluate if it is possible to choose a group of some of the ints, such that the group sums to the given target with this additional constraint: If a value in the array is chosen to be in the group, the value immediately following it in the array must not be chosen.""" in {
+    RecursionII.groupNoAdj(0, List(2, 5, 10, 4), 12) should equal(true)
+    RecursionII.groupNoAdj(0, List(2, 5, 10, 4), 14) should equal(false)
+    RecursionII.groupNoAdj(0, List(2, 5, 10, 4), 7) should equal(false)
+  }
+
 }
